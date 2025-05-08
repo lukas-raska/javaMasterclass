270b84ff5c78696f0721f63c3c2627d851774224
lukas-raska <luky.raska@gmail.com>
2025-05-08 18:52:53 +0200

    lesson 35 complete

---
+++ src/main/java/dev/lpa/section4/_35/Hello.java
@@ -0,0 +1,18 @@
+package dev.lpa.section4._35;
+
+public class Hello {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello, Tim");
+
+        boolean isAlien = false;
+
+        if (isAlien ==false) {
+            System.out.println("It is not an alien!");
+            System.out.println("And I am scared of aliens");
+        }
+
+
+    }
+}
