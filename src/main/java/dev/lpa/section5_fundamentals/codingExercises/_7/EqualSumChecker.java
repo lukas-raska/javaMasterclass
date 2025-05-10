13e142762e058baf3d4c0eaf97b259be118a4a6e
lukas-raska <luky.raska@gmail.com>
2025-05-10 07:45:46 +0200

    exercise 7 complete

---
+++ src/main/java/dev/lpa/section5_fundamentals/codingExercises/_7/EqualSumChecker.java
@@ -0,0 +1,10 @@
+package dev.lpa.section5_fundamentals.codingExercises._7;
+
+public class EqualSumChecker {
+
+    public static boolean hasEqualSum(int a,
+                                      int b,
+                                      int c) {
+        return a + b == c;
+    }
+}
