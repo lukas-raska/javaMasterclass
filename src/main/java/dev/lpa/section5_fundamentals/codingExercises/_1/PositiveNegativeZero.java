3d9afc84a09183bb962f044a118bbb6975463468
lukas-raska <luky.raska@gmail.com>
2025-05-10 06:48:35 +0200

    exercise 1 complete

---
+++ src/main/java/dev/lpa/section5_fundamentals/codingExercises/_1/PositiveNegativeZero.java
@@ -0,0 +1,8 @@
+package dev.lpa.section5_fundamentals.codingExercises._1;
+
+public class PositiveNegativeZero {
+
+    public static void checkNumber(int number) {
+        System.out.println((number == 0) ? "zero" : (number > 0) ? "positive" : "negative");
+    }
+}
