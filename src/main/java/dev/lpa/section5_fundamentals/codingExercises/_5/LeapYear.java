5a337453d4e2a8858b83d48900a4e5672407d574
lukas-raska <luky.raska@gmail.com>
2025-05-10 07:35:08 +0200

    exercise 5 complete

---
+++ src/main/java/dev/lpa/section5_fundamentals/codingExercises/_5/LeapYear.java
@@ -0,0 +1,11 @@
+package dev.lpa.section5_fundamentals.codingExercises._5;
+
+public class LeapYear {
+
+    public static boolean isLeapYear(int year) {
+        if (year < 1 || year > 9999) {
+            return false;
+        }
+        return year % 4 == 0 && year % 100 != 0 || year % 400 == 0;
+    }
+}
